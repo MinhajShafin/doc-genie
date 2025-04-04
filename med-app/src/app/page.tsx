f1ff9b94d5198afce2b4c4de9c685f44d671e9cf
Minhaj Shafin <minhajshafin@gmail.com>
2025-04-04 23:44:23 +0600

    Delete med-app directory

--- med-app/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1 className="text-8xl flex flex-row min-h-screen justify-center items-center ">
-        Work in progress!!!
-      </h1>
-    </>
-  );
-}
